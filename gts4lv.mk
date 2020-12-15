42ed91137db844ffadf8406d96688fd2cb43ed3b
Julian Fischer <myst3rion9@gmail.com>
2020-12-15 22:34:25 +0100

    gts4lv-common: Import power-libperfmgr
    - Import from https://android.googlesource.com/platform/hardware/google/pixel
    - powrhint.json: https://android.googlesource.com/device/google/bonito
    - Tag: android-11.0.0_r24

--- gts4lv.mk
+++ gts4lv.mk
@@ -255,7 +255,10 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti
+    android.hardware.power-service.pixel-libperfmgr
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json   
 
 # Protobuf
 PRODUCT_PACKAGES += \
